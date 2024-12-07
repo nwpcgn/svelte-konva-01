527c48d72a3755452f9a5b9fe82fb21a90c0122d
nwpcgn <git@stackblitz.com>
2024-12-07 10:35:37 +0100

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import {
+		Page,
+		Navbar,
+		Block,
+		Button,
+		List,
+		ListItem,
+		BlockTitle,
+		App
+	} from 'konsta/svelte'
+</script>
+
+<App theme="material" class="k-material">
+	<Page>
+		<Navbar title="My App" />
+
+		<Block strong>
+			<p>
+				Here is your SvelteKit & Konsta UI app. Let's see what we have here.
+			</p>
+		</Block>
+		<BlockTitle>Navigation</BlockTitle>
+		<List>
+			<ListItem href="/about/" title="About" />
+			<ListItem href="/form/" title="Form" />
+		</List>
+
+		<Block strong class="flex space-x-4">
+			<Button>Button 1</Button>
+			<Button>Button 2</Button>
+		</Block>
+	</Page>
+</App>
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+const konstaConfig = require('konsta/config')
+module.exports = konstaConfig({
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	darkMode: 'media', // or 'class'
+	theme: {
+		extend: {}
+	},
+	variants: {
+		extend: {}
+	},
+	plugins: []
+})
